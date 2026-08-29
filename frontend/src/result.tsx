bbd5101912677a3d59eab460e77c7044315e414f
Avichal296 <avichalshrivastava2006@gmail.com>
2026-08-29 19:36:08 +0530

    init

--- frontend/src/result.tsx
+++ frontend/src/result.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import {useParams} from "react-router"
+import { BackendUrl } from "./lib /config";
+import axios from 'axios'
 
-export const Result = ()=>{
+interface Result {
+    score: number,
+    feedback: string,
+    transcript: {type: "Assistant" | "User" , content: string}[]
+}
+export async function Result() {
+    const {id } = useParams();
+    const [Result , setResult] = useState<Result>({
+        score : 0,
+        feedback: "",
+        transcript: []
+    })
+    useEffect(()=>{
+          axios.get(`${BackendUrl}/api/v1/result/${id}`)
+         .then(response =>{
+            setResult(response.data);
+         })
+        const time =  setInterval(()=>{
+             axios.get(`${BackendUrl}/api/v1/result/${id}`)
+            .then(response =>{
+               setResult(response.data);
+            },[5*1000])
+
+            return () => {
+                clearInterval(time);
+            }
+
+         })
+    },[id])
     return (
         <div>
             <h1> result</h1>
